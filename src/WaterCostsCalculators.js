77ff82a8649259933346fb25dffb750f22c7b848
Swarup Mahapatra <swarup@socash.sg>
2022-03-21 10:20:11 +0530

    Rename WaterCostsCaclculator implementing classes

--- src/WaterCostsCalculators.js
+++ src/WaterCostsCalculators.js
@@ -1,5 +1,5 @@
 export class WaterCostsCalculator {
-  calculatorFor(waterInLiters) {
+  calculateFor(waterInLiters) {
     throw new Error('Method not implemented');
   }
 }
@@ -8,19 +8,19 @@ const validateWaterUnits = (waterInLiters) => {
   if (!Number.isInteger(waterInLiters)) {
     throw new Error('Water is not integer');
   }
-}
+};
 
-export class CorporationWaterCalculator extends WaterCostsCalculator {
-  calculatorFor(waterInLiters) {
-    validateWaterUnits(waterInLiters)
+export class CorporationWaterCostsCalculator extends WaterCostsCalculator {
+  calculateFor(waterInLiters) {
+    validateWaterUnits(waterInLiters);
     const cost = 1;
     return cost * waterInLiters;
   }
 }
 
-export class BorewellWaterCalculator extends WaterCostsCalculator {
-  calculatorFor(waterInLiters) {
-    validateWaterUnits(waterInLiters)
+export class BorewellWaterCostsCalculator extends WaterCostsCalculator {
+  calculateFor(waterInLiters) {
+    validateWaterUnits(waterInLiters);
     const cost = 1.5;
     return cost * waterInLiters;
 
@@ -49,15 +49,15 @@ class Slab {
   }
 }
 
-export class TankerWaterCalculator extends WaterCostsCalculator {
+export class TankerWaterCostsCalculator extends WaterCostsCalculator {
   SLABS = [
     new Slab({ minLiters: 1, maxLiters: 500, cost: 2 }),
     new Slab({ minLiters: 501, maxLiters: 1500, cost: 3 }),
     new Slab({ minLiters: 1501, maxLiters: 3000, cost: 5 }),
-    new Slab({ minLiters: 3001, maxLiters: null, cost: 8}),
-  ]
-  calculatorFor(waterInLiters) {
-    validateWaterUnits(waterInLiters)
+    new Slab({ minLiters: 3001, maxLiters: null, cost: 8 }),
+  ];
+  calculateFor(waterInLiters) {
+    validateWaterUnits(waterInLiters);
     let cost = 0;
     for (let slab of this.SLABS) {
       cost += slab.calculate(waterInLiters);
--- test/WaterCostsCalculators.test.js
+++ test/WaterCostsCalculators.test.js
@@ -1,105 +1,105 @@
-const { WaterCostsCalculator, CorporationWaterCalculator, BorewellWaterCalculator, TankerWaterCalculator } = require("../src/WaterCostsCalculators");
+const { WaterCostsCalculator, CorporationWaterCostsCalculator, BorewellWaterCostsCalculator, TankerWaterCostsCalculator } = require("../src/WaterCostsCalculators");
 
 describe('Water Costs Calculator', () => {
   describe('interfaces', () => {
     it('should throw error if calculaterFor implemented', () => {
       class SomeCalculator extends WaterCostsCalculator { }
-      expect(new SomeCalculator().calculatorFor).toThrow()
-    })
+      expect(new SomeCalculator().calculateFor).toThrow();
+    });
 
     it('should not throw error if calculaterFor is not implemented', () => {
-      class SomeCalculator extends WaterCostsCalculator { 
-        calculatorFor(){}
+      class SomeCalculator extends WaterCostsCalculator {
+        calculateFor() { }
       }
-      expect(new SomeCalculator().calculatorFor).not.toThrow()
+      expect(new SomeCalculator().calculateFor).not.toThrow();
 
-    })
-  })
-});
-
-describe('CorporationWaterCalculator', () => {
-  describe('calculateFor', () => {
-    it('should throw error if water is not integer', () => {
-      expect(() => { new CorporationWaterCalculator().calculatorFor(10.5); }).toThrow();
-    })
-    it('should calculate for flat rate of 1', () => {
-      expect(new CorporationWaterCalculator().calculatorFor(10)).toEqual(10);
-    })
-  })
-});
-
-describe('BorewellWaterCalculator', () => {
-  describe('calculateFor', () => {
-    it('should throw error if water is not integer', () => {
-      expect(() => { new BorewellWaterCalculator().calculatorFor(10.5); }).toThrow();
-    })
-    it('should calculate for flat rate of 1.5', () => {
-      expect(new BorewellWaterCalculator().calculatorFor(10)).toEqual(15);
-    });
-  })
-});
-
-describe('BorewellWaterCalculator', () => {
-  describe('calculateFor', () => {
-    it('should throw error if water is not integer', () => {
-      expect(() => { new BorewellWaterCalculator().calculatorFor(10.5); }).toThrow();
-    });
-    it('should calculate for flat rate of 1.5', () => {
-      expect(new BorewellWaterCalculator().calculatorFor(10)).toEqual(15);
     });
   });
 });
 
-describe('TankerWaterCalculator', () => {
+describe('CorporationWaterCostsCalculator', () => {
   describe('calculateFor', () => {
     it('should throw error if water is not integer', () => {
-      expect(() => { new TankerWaterCalculator().calculatorFor(10.5); }).toThrow();
+      expect(() => { new CorporationWaterCostsCalculator().calculateFor(10.5); }).toThrow();
+    });
+    it('should calculate for flat rate of 1', () => {
+      expect(new CorporationWaterCostsCalculator().calculateFor(10)).toEqual(10);
+    });
+  });
+});
+
+describe('BorewellWaterCostsCalculator', () => {
+  describe('calculateFor', () => {
+    it('should throw error if water is not integer', () => {
+      expect(() => { new BorewellWaterCostsCalculator().calculateFor(10.5); }).toThrow();
+    });
+    it('should calculate for flat rate of 1.5', () => {
+      expect(new BorewellWaterCostsCalculator().calculateFor(10)).toEqual(15);
+    });
+  });
+});
+
+describe('BorewellWaterCostsCalculator', () => {
+  describe('calculateFor', () => {
+    it('should throw error if water is not integer', () => {
+      expect(() => { new BorewellWaterCostsCalculator().calculateFor(10.5); }).toThrow();
+    });
+    it('should calculate for flat rate of 1.5', () => {
+      expect(new BorewellWaterCostsCalculator().calculateFor(10)).toEqual(15);
+    });
+  });
+});
+
+describe('TankerWaterCostsCalculator', () => {
+  describe('calculateFor', () => {
+    it('should throw error if water is not integer', () => {
+      expect(() => { new TankerWaterCostsCalculator().calculateFor(10.5); }).toThrow();
     });
     describe('slabs', () => {
       it('for 0 liters, cost is 0 ', () => {
-        expect(new TankerWaterCalculator().calculatorFor(0)).toEqual(0);
-      })
+        expect(new TankerWaterCostsCalculator().calculateFor(0)).toEqual(0);
+      });
       describe('rate for minLiters: 1, maxLiters: 500, cost: 2', () => {
         it('for 1 liter, cost is 2 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(1)).toEqual(2);
+          expect(new TankerWaterCostsCalculator().calculateFor(1)).toEqual(2);
         });
         it('for 100 liter, cost is 200 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(100)).toEqual(200);
+          expect(new TankerWaterCostsCalculator().calculateFor(100)).toEqual(200);
         });
         it('for 500 liter, cost is 1000 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(500)).toEqual(1000);
-        })
-      })
+          expect(new TankerWaterCostsCalculator().calculateFor(500)).toEqual(1000);
+        });
+      });
       describe('rate for minLiters: 501, maxLiters: 1500, cost: 3', () => {
         it('for 501 liter, cost is 1003 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(501)).toEqual(1003);
+          expect(new TankerWaterCostsCalculator().calculateFor(501)).toEqual(1003);
         });
         it('for 1000 liter, cost is 2500 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(1000)).toEqual(2500);
+          expect(new TankerWaterCostsCalculator().calculateFor(1000)).toEqual(2500);
         });
         it('for 1500 liter, cost is 4000 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(1500)).toEqual(4000);
-        })
-      })
+          expect(new TankerWaterCostsCalculator().calculateFor(1500)).toEqual(4000);
+        });
+      });
       describe('rate for minLiters: 1501, maxLiters: 3000, cost: 5', () => {
         it('for 1501 liter, cost is 4005 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(1501)).toEqual(4005);
+          expect(new TankerWaterCostsCalculator().calculateFor(1501)).toEqual(4005);
         });
         it('for 2000 liter, cost is 6500 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(2000)).toEqual(6500);
+          expect(new TankerWaterCostsCalculator().calculateFor(2000)).toEqual(6500);
         });
         it('for 3000 liter, cost is 11500 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(3000)).toEqual(11500);
-        })
-      })
+          expect(new TankerWaterCostsCalculator().calculateFor(3000)).toEqual(11500);
+        });
+      });
       describe('minLiters: 3001, maxLiters: null, cost: 8', () => {
         it('for 3001 liter, cost is 11508 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(3001)).toEqual(11508);
+          expect(new TankerWaterCostsCalculator().calculateFor(3001)).toEqual(11508);
         });
         it('for 4000 liter, cost is 19500 ', () => {
-          expect(new TankerWaterCalculator().calculatorFor(4000)).toEqual(19500);
-        })
-      })
-    })
+          expect(new TankerWaterCostsCalculator().calculateFor(4000)).toEqual(19500);
+        });
+      });
+    });
   });
 });
